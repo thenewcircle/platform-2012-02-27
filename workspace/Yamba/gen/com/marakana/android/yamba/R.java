8300185fa5113b6c6cd10c541ed5502146b5ece2
Ken Jones <ken.jones@marakana.com>
2012-02-28 12:35:24 +0100

    Completed layout; added OnClickListener

--- workspace/Yamba/gen/com/marakana/android/yamba/R.java
+++ workspace/Yamba/gen/com/marakana/android/yamba/R.java
@@ -13,11 +13,20 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button_update=0x7f060001;
+        public static final int edit_msg=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int activity_status_title=0x7f040002;
         public static final int app_name=0x7f040001;
+        public static final int button_update_text=0x7f040003;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int activity_title=0x7f050000;
     }
 }
--- workspace/Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ workspace/Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -2,12 +2,30 @@ package com.marakana.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button buttonUpdate = (Button) findViewById(R.id.button_update);
+        buttonUpdate.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.button_update:
+			// Update button clicked
+			break;
+		default:
+			// We should never get here!
+		}
+	}
 }
