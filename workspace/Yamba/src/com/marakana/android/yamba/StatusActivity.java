985ecce8a780f932d5fac4dd006248f3afacea14
Ken Jones <ken.jones@marakana.com>
2012-02-28 13:58:23 +0100

    Implemented button click processing; added logging

--- workspace/Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ workspace/Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -2,17 +2,24 @@ package com.marakana.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class StatusActivity extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */
+    private static final String TAG = "StatusActivity";
+    
+    private EditText mStatusMsg;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        mStatusMsg = (EditText) findViewById(R.id.edit_msg);
         Button buttonUpdate = (Button) findViewById(R.id.button_update);
         buttonUpdate.setOnClickListener(this);
     }
@@ -23,6 +30,10 @@ public class StatusActivity extends Activity implements OnClickListener {
 		switch (id) {
 		case R.id.button_update:
 			// Update button clicked
+			Log.v(TAG, "Button clicked");
+			String msg = mStatusMsg.getText().toString();
+			Log.v(TAG, "User entered: " + msg);
+			mStatusMsg.setText("");
 			break;
 		default:
 			// We should never get here!
